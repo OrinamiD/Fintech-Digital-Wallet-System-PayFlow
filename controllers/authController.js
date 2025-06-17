5066d1afa473e58f2ce49fc7c5e306b8de8f7d1f
OrinamiD <dongoorinami@gmail.com>
2025-06-17 17:26:52 +0100

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,198 +1,189 @@
-
-
-
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const Transaction = require("../models/transactionModel")
-const User = require("../models/userModel")
-const Wallet = require("../models/walletModel")
-const forgotPasswordEmail = require("../sendEmails/forgotPassword")
-const { registrationEmail, } = require("../sendEmails/registrationEmail")
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Transaction = require("../models/transactionModel");
+const User = require("../models/userModel");
+const Wallet = require("../models/walletModel");
+const forgotPasswordEmail = require("../sendEmails/forgotPassword");
+const { registrationEmail } = require("../sendEmails/registrationEmail");
 
 // welcome message
-const handleWelcomeMessage = async (req, res)=>{
-   try {
-    
-     return res.status(200).json({message: "Welcome to Fintech Digital Wallet System "})
-
-   } catch (error) {
-     return res.status(200).json({message: error.message})
-   }
-}
+const handleWelcomeMessage = async (req, res) => {
+  try {
+    return res
+      .status(200)
+      .json({ message: "Welcome to Fintech Digital Wallet System " });
+  } catch (error) {
+    return res.status(200).json({ message: error.message });
+  }
+};
 
 // sign-up
-const handleUserRegistration = async (req, res)=>{
+const handleUserRegistration = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-    
-        try {
-        
-            
-        const { name, email, password} = req.body
-    
-    
-        const existingUser = await User.findOne({ email })
-    
-        if(existingUser){
-            return res.status(404).json({message: "User account already exist. login"})
-        }
-    
-    
-        const hashedPassword = await bcrypt.hash(password, 12)
-    
-        const newUser = new User({
-            name,
-            email,
-            password: hashedPassword
-            })
-    
-            const result =  await newUser.save()
-            result.password = undefined;
-    
-            const wallet = new Wallet({
-                user_id: newUser?._id,
-                balance: 0})
-                
-                await wallet.save()
-                
+    const existingUser = await User.findOne({ email });
 
-                
-                // send email
-await registrationEmail(name, email, password)
-
-
-            return res.status(201).json({message: "Registration successfull",
-                result,
-                wallet
-            })
-            
-    
-        } catch (error) {
-            return res.status(500).json({message: error.message})
-        }
+    if (existingUser) {
+      return res
+        .status(404)
+        .json({ message: "User account already exist. login" });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const result = await newUser.save();
+    result.password = undefined;
+
+    const wallet = new Wallet({
+      user_id: newUser?._id,
+      balance: 0,
+    });
+
+    await wallet.save();
+
+    // send email
+    await registrationEmail(name, email, password);
+
+    return res
+      .status(201)
+      .json({ message: "Registration successfull", result, wallet });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 // login
-const handleUserLogin = async (req, res)=>{
+const handleUserLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    const user = await User.findOne({ email });
 
-    try {
-  
-        const { email, password } = req.body
-
-        const user = await User.findOne({ email })
-
-        if(!user){
-            return res.status(404).json({message: "User account not found "})
-        }
-
-        const ismatch = await bcrypt.compare(password, user?.password)
-
-        if(!ismatch){
-            return res.status(400).json({message: "Incorrect password or email"})
-        }
-
-        const accessToken = jwt.sign({user_id: user?._id}, `${process.env.ACCESS_TOKEN}`, {expiresIn: "7m"})
-
-        const refreshToken = jwt.sign({user_id: user?._id}, `${process.env.REFRESH_TOKEN}`, {expiresIn: "7d"})
-
-        return res.status(200).json({message: "Login successfull",
-          user,
-          accessToken
-        },
-
-        refreshToken
-    )
-
-    } catch (error) {
-         return res.status(500).json({message: error.message})
+    if (!user) {
+      return res.status(404).json({ message: "User account not found " });
     }
-    
-}
+
+    const ismatch = await bcrypt.compare(password, user?.password);
+
+    if (!ismatch) {
+      return res.status(400).json({ message: "Incorrect password or email" });
+    }
+
+    const accessToken = jwt.sign(
+      { user_id: user?._id },
+      `${process.env.ACCESS_TOKEN}`,
+      { expiresIn: "7m" }
+    );
+
+    const refreshToken = jwt.sign(
+      { user_id: user?._id },
+      `${process.env.REFRESH_TOKEN}`,
+      { expiresIn: "7d" }
+    );
+
+    return res.status(200).json(
+      { message: "Login successfull", user, accessToken },
+
+      refreshToken
+    );
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 // forgot password
-const handleForgotPassword = async (req, res)=>{
+const handleForgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
 
-   try {
+    const user = await User.findOne({ email });
 
-     const {email} = req.body
-
-    const user = await User.findOne({email})
-
-    if(!user){
-         return res.status(404).json({message: "User account not found"})
+    if (!user) {
+      return res.status(404).json({ message: "User account not found" });
     }
 
     // send message with token
-const accessToken = jwt.sign({user_id: user?._id}, `${process.env.ACCESS_TOKEN}`, {expiresIn: "5m"})
+    const accessToken = jwt.sign(
+      { user_id: user?._id },
+      `${process.env.ACCESS_TOKEN}`,
+      { expiresIn: "5m" }
+    );
 
-await forgotPasswordEmail(email, accessToken)
+    await forgotPasswordEmail(email, accessToken);
 
-return res.status(200).json({message: "Please check your email inbox"})
+    return res.status(200).json({ message: "Please check your email inbox" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-    
-   } catch (error) {
-    return res.status(500).json({message: error.message})
-   }
- }
+// reset password
+const handleResetPassword = async (req, res) => {
+  try {
+    console.log(user.req);
 
- // reset password
-const handleResetPassword = async (req, res)=>{
+    const { password } = req.body;
 
-    try {
-        console.log(user.req)
-        
-        const {password} = req.body
+    const user = await User.findOne({ id: req.user?._id });
 
-        const user = await User.findOne({id: req.user?._id})
-
-        if(!user){
-            return res.status(404).json({message: "User account not found"})
-        }
-
-        const hashedpassword = await bcrypt.hash(password, 12)
-
-        // await User.findOneAndUpdate({id: req.user?._id}, {password: hashedpassword})
-
-        user.password = hashedpassword
-
-        await user.save()
-
-         const accessToken = jwt.sign({user: user_id}, `${process.env.ACCESS_TOKEN}`, {expiresIn: "5m"})
-
-         const refreshToken = jwt.sign({user: user_id}, `${process.env.REFRESH_TOKEN}`, {expiresIn: "7d"})
-
-         return res.status(200).json({message: "Password Reset successful", accessToken, user},
-
-            refreshToken
-         )
-
-    
-    } catch (error) {
-        return res.status(500).json({message: error.message})
+    if (!user) {
+      return res.status(404).json({ message: "User account not found" });
     }
 
-}
+    const hashedpassword = await bcrypt.hash(password, 12);
+
+    // await User.findOneAndUpdate({id: req.user?._id}, {password: hashedpassword})
+
+    user.password = hashedpassword;
+
+    await user.save();
+
+    const accessToken = jwt.sign(
+      { user: user_id },
+      `${process.env.ACCESS_TOKEN}`,
+      { expiresIn: "5m" }
+    );
+
+    const refreshToken = jwt.sign(
+      { user: user_id },
+      `${process.env.REFRESH_TOKEN}`,
+      { expiresIn: "7d" }
+    );
+
+    return res.status(200).json(
+      { message: "Password Reset successful", accessToken, user },
+
+      refreshToken
+    );
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 // const handleLogOut = async (req, res)=>{
 
 //     try {
-        
+
 //         res.clearCookie('refreshToken', {path: '/user/refresh_token'})
 //         return res.status(200).json({message: "Logged out."})
 //     } catch (error) {
-        
+
 //         return res.status(500).json({message: error.message})
 //     }
 // }
 
-
 module.exports = {
-     handleUserRegistration,
-     handleUserLogin,
-     handleForgotPassword,
-     handleResetPassword,
-     handleWelcomeMessage,
-    //  handleLogOut
-
-}
+  handleUserRegistration,
+  handleUserLogin,
+  handleForgotPassword,
+  handleResetPassword,
+  handleWelcomeMessage,
+  //  handleLogOut
+};
--- middleware/index.js
+++ middleware/index.js
@@ -40,27 +40,48 @@ const validateRegistration = async (req, res, next)=>{
             return res.status(401).json({message: errors})
         }
 
-    //     const signupSchema = Joi.object({
-    //   email: Joi.string()
-    //     .min(6)
-    //     .max(60)
-    //     .email({ tlds: { allow: ['com', 'net'] } })
-    //     .required(),
+    const signupSchema = joi.object({
+       
+        name: joi.string()
+        .required()
+        .pattern(new RegExp("^[A-Za-z\s'-]{2,}$"))
+        .messages({
+            'string.pattern.base': "Name must contain only alphabetic characters, spaces, apostrophes, or hyphens. Numbers and special symbols are not allowed.",
+           
 
-    //   password: Joi.string()
-    //     .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&^_-])[A-Za-z\d@$!%*?#&^_-]{8,}$/)
-    //     .required()
-    //     .messages({
-    //       "string.pattern.base":
-    //         "Password must contain at least 8 characters, including uppercase, lowercase, number, and special character.",
-    //     }),
-    // });
+        }),            
 
-    // const  { error } = signupSchema.validate({ email, password });
 
-    // if (error) {
-    //   return res.status(400).json({ message: error.message });
-    // }
+        email: joi.string()
+        .required()
+        .min(4)
+        .max(60)
+        .pattern(new RegExp("^[^@]+@[^@]+\.[^@]+$"))
+        .messages({
+        'string.pattern.base': "Please enter a valid email address (e.g., name@example.com).",
+        'string.email': "Please enter a valid email address.",
+      
+    }),
+
+
+        password: joi.string()
+
+        .required()
+        .pattern(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"))
+        .messages({
+            'string.pattern.base': "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character.",
+          
+
+        })
+
+        
+    })
+
+    const { error } = signupSchema.validate({ name, email, password})
+
+    if(error){
+        return res.status(400).json({ message: error.message})
+    }
                    
 next()
 
