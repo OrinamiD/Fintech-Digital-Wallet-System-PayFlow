5cfec3217c018efc35ce2228c24e6e7e36dadf61
OrinamiD <dongoorinami@gmail.com>
2025-06-21 22:35:18 +0100

    updated

--- sendEmails/registrationEmail.js
+++ sendEmails/registrationEmail.js
@@ -14,23 +14,17 @@ const registrationEmail = async (email, token) => {
     to: `${email}`,
     subject: "Registration Successful",
     html: `
-    <h2>Welcome to E-Commerce Superstore!</h2>
-    <p>We're excited to have you join us. Please verify your email to complete your registration.</p>
+    <h2>Welcome to Fintech Digital Wallet System!</h2>
+    <p>We're excited to have you join us.</p>
 
     <p>
-        <a href="https://www.yourcareerex.com/verify-email/${token}" style="background-color:#28a745;color:white;padding:10px 20px;text-decoration:none;border-radius:5px;">Verify Email</a>
-    </p>
-
-    <p>If the button doesn't work, please use the link below:</p>
-    <p>
-        <a href="https://www.yourcareerex.com/verify-email/${token}">https://www.yourcareerex.com/verify-email/${token}</a>
-    </p>
+       
 
     <hr>
 
     
 
-    <p>Thank you,<br/>The E-Commerce Superstore Team</p>
+    <p>Thank you,<br/>The Fintech Team</p>
 `
 
     
