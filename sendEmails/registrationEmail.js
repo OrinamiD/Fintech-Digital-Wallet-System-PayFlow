d8daf1b2590669af12e34e1047f48dabfbb74022
OrinamiD <dongoorinami@gmail.com>
2025-06-21 22:20:18 +0100

    updated

--- sendEmails/registrationEmail.js
+++ sendEmails/registrationEmail.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const registrationEmail = async (name, email, password) => {
+const registrationEmail = async (email, token) => {
   const mailTransport = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -13,7 +13,27 @@ const registrationEmail = async (name, email, password) => {
     from: `${process.env.EMAIL}`,
     to: `${email}`,
     subject: "Registration Successful",
-    html: `<h1>Welcome to Fintech Digital Wallet System</h1>`,
+    html: `
+    <h2>Welcome to E-Commerce Superstore!</h2>
+    <p>We're excited to have you join us. Please verify your email to complete your registration.</p>
+
+    <p>
+        <a href="https://www.yourcareerex.com/verify-email/${token}" style="background-color:#28a745;color:white;padding:10px 20px;text-decoration:none;border-radius:5px;">Verify Email</a>
+    </p>
+
+    <p>If the button doesn't work, please use the link below:</p>
+    <p>
+        <a href="https://www.yourcareerex.com/verify-email/${token}">https://www.yourcareerex.com/verify-email/${token}</a>
+    </p>
+
+    <hr>
+
+    
+
+    <p>Thank you,<br/>The E-Commerce Superstore Team</p>
+`
+
+    
   };
 
   await mailTranspot.sendMail(emailDetails);
